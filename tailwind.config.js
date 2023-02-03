ea687123ddd33d0af71be99673cb909bb5765bf3
rachel tang <82250973+anjushuu@users.noreply.github.com>
2023-02-03 00:47:12 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './P1/src/**/*.html',
@@ -10,17 +11,29 @@ module.exports = {
       darkgreen: '#748F6F',
       midgreen: '#A4BF9F',
       lightgray: '#D9DED7',
+      lightblue: 'C7D6E0',
     },
+    dropShadow: {
+      DEFAULT: '0 4px 4px rgba(0, 0, 0, 0.25))',
+    },
+
     extend: {
       borderRadius: {
         DEFAULT: '10px',
       },
 
       fontFamily: {
-        'sans': ['Inter']
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+
+      fontSize: {
+        'xs': '10px',
       }
     }
   },
 
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+
+  ],
 }
